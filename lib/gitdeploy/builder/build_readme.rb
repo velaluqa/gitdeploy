74eae4989724c0b9117b78b343bd6a32dd9ca851
Arthur Leonard Andersen <leoc.git@gmail.com>
2015-01-20 22:37:39 +0100

    Fix building readme for ill formatted tags
    We use `Gem::Version` to parse tags. Ill formatted or free-form tags
    caused an `ArgumentError` when building readme. Now those tags are
    simply ignored when generating the CHANGELOG.

--- lib/gitdeploy/builder/build_readme.rb
+++ lib/gitdeploy/builder/build_readme.rb
@@ -2,27 +2,32 @@ module BuildReadme
   def build_readme
     changelog = "## Changelog\n"
     tags = `git tag -l`.split("\n")
-    tags.map! { |v| Gem::Version.new(v) }
-    tags.sort!
-    tags.reverse_each do |tag|
-      meta, message = `git cat-file -p \`git rev-parse #{tag}\``.split("\n\n", 2)
-      if meta.include?('tagger ')
-        changelog << "\n"
-        changelog << "### #{tag}\n"
-        changelog << "\n"
-        changelog << message
+    tags.map! do |v|
+      begin
+        Gem::Version.new(v)
+      rescue StandardError
+        # The tag might not be of correct version format.
+        # In those cases we simply ignore the tag (remove them by compact!).
+        nil
       end
     end
+    tags
+      .compact!
+      .sort!
+    tags.reverse_each do |tag|
+      meta, message = `git cat-file -p \`git rev-parse #{tag}\``.split("\n\n", 2)
+      changelog << "\n### #{tag}\n\n#{message}" if meta.include?('tagger ')
+    end
     File.open('CHANGELOG', 'w+') { |f| f << changelog }
-    if File.exists?('README.md')
+    if File.exist?('README.md')
       `git checkout -- README.md`
       File.open('README.md', 'a+') { |f| f << "\n" << changelog }
     end
   end
 
   def clean_readme
-    puts "Removing CHANGELOG"
+    puts 'Removing CHANGELOG'
     FileUtils.rm_f('CHANGELOG')
-    `git checkout -- README.md` if File.exists?('README.md')
+    `git checkout -- README.md` if File.exist?('README.md')
   end
 end
