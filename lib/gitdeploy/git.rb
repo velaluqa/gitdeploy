5d1ca9572ae0f00735ce4d2dcfdfdd3fe5aeb481
Franz Kißig <h@entire.network>
2015-04-16 14:35:46 +0200

    Move Git class into Gitdeploy module

--- lib/gitdeploy/git.rb
+++ lib/gitdeploy/git.rb
@@ -1,31 +1,33 @@
 require 'English'
 
-class Git
-  class << self
-    def pwd
-      @pwd ||= ::File.expand_path(::Dir.pwd)
-    end
+module Gitdeploy
+  class Git
+    class << self
+      def pwd
+        @pwd ||= ::File.expand_path(::Dir.pwd)
+      end
 
-    def branch
-      @branch ||= (ENV['GIT_BRANCH'] || `cd #{pwd}; git rev-parse --abbrev-ref HEAD`.strip)
-    end
+      def branch
+        @branch ||= (ENV['GIT_BRANCH'] || `cd #{pwd}; git rev-parse --abbrev-ref HEAD`.strip)
+      end
 
-    def branch_name
-      @branch_name ||= branch.gsub(%r{^[^/]+/}, '')
-    end
+      def branch_name
+        @branch_name ||= branch.gsub(%r{^[^/]+/}, '')
+      end
 
-    def tag
-      stdout = `cd #{pwd}; git describe --tags 2>&1`.strip
-      status = $CHILD_STATUS
-      @tag ||= stdout.strip if status.exitstatus == 0
-    end
+      def tag
+        stdout = `cd #{pwd}; git describe --tags 2>&1`.strip
+        status = $CHILD_STATUS
+        @tag ||= stdout.strip if status.exitstatus == 0
+      end
 
-    def rev
-      @rev ||= (ENV['GIT_COMMIT'] || `cd #{pwd}; git rev-parse HEAD`.strip)
-    end
+      def rev
+        @rev ||= (ENV['GIT_COMMIT'] || `cd #{pwd}; git rev-parse HEAD`.strip)
+      end
 
-    def tagged?
-      tag && tag != '' && tag !~ /^(.*)-([0-9]+)-([0-9a-z]+)$/
+      def tagged?
+        tag && tag != '' && tag !~ /^(.*)-([0-9]+)-([0-9a-z]+)$/
+      end
     end
   end
 end
